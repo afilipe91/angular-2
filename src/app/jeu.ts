14e5bf968146763833e271ff0af44d7a011700db
Adrien <adrien@filipefamily.fr>
2017-01-27 17:52:43 +0100

    Exercice 2 - Class Jeu ok

--- src/app/jeu.ts
+++ src/app/jeu.ts
@@ -1,11 +1,31 @@
+// export class Jeu {
+//     screenshots: Array<string>;
+//     cover: Array<string>;
+//
+//     constructor(public name:string, public summary:string, public genres:number){
+//         for(let screenshot of screenshots) {
+//             this.screenshots = screenshots.url;
+//         }
+//         this.cover = cover.url;
+//     }
+// }
 export class Jeu {
-    screenshots: Array<string>;
-    cover: Array<string>;
+    name;
+    summary;
+    genres;
+    cover;
+    screenshots;
+    constructor(data)
+    {
+        Object.assign(this, data);
+    }
 
-    constructor(public name:string, public summary:string, public genres:number){
-        for(let screenshot of screenshots) {
-            this.screenshots = screenshots.url;
+    getScreenshots()
+    {
+        if(this.screenshots != null) {
+            return this.screenshots.map(screenshot => screenshot.url);
+        } else {
+            return [];
         }
-        this.cover = cover.url;
     }
 }
